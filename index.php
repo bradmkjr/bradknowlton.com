ddce56309686e559aefd7f3f094974c0c51cd664
Bradmkjr <brad@ucanbonline.net>
2019-12-25 12:25:01 -0700

    Updated social icons

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -16,13 +15,13 @@
 <meta property="og:type" content="article"/>
 <meta property="og:url" content="https://www.bradknowlton.com/"/>
 <meta property="og:image" content="https://www.bradknowlton.com/images/bradknowlton.jpg"/>
-<meta property="og:description" content="Profile of Bradford M. Knowlton Jr. currently programming PHP in Missoula, Montana"/> 
+<meta property="og:description" content="Profile of Bradford M. Knowlton Jr. currently programming PHP in Missoula, Montana"/>
 
 <meta name="webstatsdomain.org_meta" content="bradknowlton.com"/>
 
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="/tools/style.css" rel="stylesheet" type="text/css"/>
-<script type="text/javascript" src="/tools/jquery.min.js"></script> 
+<script type="text/javascript" src="/tools/jquery.min.js"></script>
 <meta name="viewport" content="width=device-width, user-scalable=no">
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -40,28 +39,63 @@
 <body>
 	<div class="icon-wrapper">
 		<ul class="icons">
-			<li class="bitcoin"><a href="bitcoin:195jHGYxZyYxxHAYgLVGY2H9DEoGMHGQTx" target="_blank"><i class="fa fa-btc fa-fw"></i></a></li>
-			<li class="twitter"><a href="https://twitter.com/bradmkjr" target="_blank"><i class="fa fa-twitter fa-fw"></i></a></li>
-			<li class="paypal"><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=brad%40ucanbonline%2enet&amp;lc=US&amp;item_name=Bradford%20Knowlton&amp;item_number=bradknowlton%2ecom&amp;no_note=1&amp;no_shipping=1¤cy_code=USD&amp;bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted" target="_blank"><i class="fa fa-paypal fa-fw"></i></a></li>
-			<li class="wordpress"><a href="http://profiles.wordpress.org/bradmkjr/" target="_blank"><i class="fa fa-wordpress fa-fw"></i></a></li>
-			<li class="ubuntu"><a href="http://ubuntuforums.org/member.php?u=508951&amp;tab=aboutme#aboutme" target="_blank"><i class="fa fa-github fa-fw"></i></a></li>
-			<li class="downloads"><a href="/images/wallpaper-original.jpg" target="_blank" title="Download The Best Super Beetle Wallpaper"><i class="fa fa-download fa-fw"></i></a></li>
+			<li class="bitcoin">
+				<a href="bitcoin:195jHGYxZyYxxHAYgLVGY2H9DEoGMHGQTx" target="_blank">
+					<i class="fa fa-btc fa-fw"></i>
+				</a>
+			</li>
+			<li class="twitter">
+				<a href="https://twitter.com/bradmkjr" target="_blank">
+					<i class="fa fa-twitter fa-fw"></i>
+				</a>
+			</li>
+			<li class="wordpress">
+				<a href="http://profiles.wordpress.org/bradmkjr/" target="_blank">
+					<i class="fa fa-wordpress fa-fw"></i>
+				</a>
+			</li>
+			<li class="github">
+				<a href="https://github.com/bradmkjr" target="_blank">
+					<i class="fab fa-github"></i>
+				</a>
+			</li>
+			<li class="bitbucket">
+				<a href="https://bitbucket.org/bradmkjr/" target="_blank">
+					<i class="fab fa-bitbucket"></i>
+				</a>
+			</li>
+			<li class="npm">
+				<a href="https://www.npmjs.com/~bradmkjr" target="_blank">
+					<i class="fab fa-npm"></i>
+				</a>
+			</li>
+			<li class="packagist">
+				<a href="https://packagist.org/packages/bradmkjr/" target="_blank">
+					<i class="fab fa-php"></i>
+				</a>
+			</li>
+			<li class="ubuntu">
+				<a href="http://ubuntuforums.org/member.php?u=508951&amp;tab=aboutme#aboutme" target="_blank">
+					<i class="fab fa-ubuntu fa-fw"></i>
+				</a>
+			</li>
+			<li class="downloads">
+				<a href="/images/wallpaper-original.jpg" target="_blank" title="Download The Best Super Beetle Wallpaper">
+					<i class="fa fa-download fa-fw"></i>
+				</a>
+			</li>
 			<!-- <li><a href="https://plus.google.com/u/0/112285836444683382716/?rel=author"><i class="fa fa-google-plus fa-fw"></i></a></li> -->
 			<!-- <li><a href="http://nsfwpassword.com/"><i class="fa fa-lock fa-fw"></i></a></li> -->
 			<li><a href="http://php75n2hzj4ucyq4.onion/"><i class="fa fa-code fa-fw"></i></a></li>
 		</ul>
-		
 	</div>
 <div id="content_wrapper">
 
 	<div class="content">
-		
 		<div class="clear"></div>
 		<p class="made_in">Bradford M. Knowlton Jr.</p>
 		<h1><a href="http://php75n2hzj4ucyq4.onion/" target="_blank" title="Tor HTTP Service is hosted on Raspberry Pi, please be patience while loading.">http://php75n2hzj4ucyq4.onion/</a></h1>
 		<h3 class="quote">You were the highest paid, least reliable employee I've ever had.<div style="text-align: right; font-size: .75em;">&mdash; Gabriel Silverman</div></h3>
-		
-		
 	</div>
 </div>
 <div id="downloads">
