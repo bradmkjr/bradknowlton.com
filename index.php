4d2542d30895a1677335ca75580e7099fe69f1f6
Bradmkjr <brad@ucanbonline.net>
2019-12-25 12:34:58 -0700

    Fixed SSL Redirect

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' ||
+   $_SERVER['HTTPS'] == 1) ||
+   isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+   $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
     $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
